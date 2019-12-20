069518c279cad0695a75d24b29b866623c86c305
许琦 <splendid@xuqideMacBook-Pro.local>
2019-12-20 20:06:51 +0800

    fix some bugs in using docker-compose

--- server/models/consts.go
+++ server/models/consts.go
@@ -9,7 +9,7 @@ const (
 	KindSalerInt    = 1
 	KindCustomerStr = "customer"
 	KindSalerStr    = "saler"
-	NatsUrl = "nats://127.0.0.1:4222"
+	NatsUrl = "nats:4222"
 	AssignCoupon_Subj = "AssignCounpon"
 	AssignCounpon_SubscribeNum = 4
 )
